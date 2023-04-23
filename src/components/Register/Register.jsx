b0efd49d1dcfcaae0c3c1fe99e9c05aef52c71a3
Touhidur Zaman <touhidcodes@gmail.com>
2023-04-23 22:53:32 +0600

    forms-input-change

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+
+// const [pass, setPass] = useState("");
 
 const Register = () => {
+	const [email, setEmail] = useState("");
+	const handleEmailChange = (event) => {
+		console.log(event.target.value);
+		setEmail(event.target.value);
+	};
+
+	const handlePasswordChange = (e) => {
+		console.log(e.target.value);
+	};
+
+	const handleSubmit = (e) => {
+		event.preventDefault();
+		const email = e.target.email.value;
+		const pass = e.target.password.value;
+		console.log(email, pass);
+	};
 	return (
 		<div>
 			<h3>Please Register</h3>
-			<form action=''>
+			<form action='' onSubmit={handleSubmit}>
 				<input
 					type='email'
 					name='email'
 					id='email'
 					placeholder='Input your email'
+					onChange={handleEmailChange}
 				/>
 				<br />
 				<input
@@ -17,6 +36,7 @@ const Register = () => {
 					name='password'
 					id='password'
 					placeholder='Input your Password'
+					onBlur={handlePasswordChange}
 				/>
 				<br />
 				<input type='submit' value='Register' />
