0165cea086c2437ed777f86c1a977747e0556e55
Touhidur Zaman <touhidcodes@gmail.com>
2023-04-25 12:45:29 +0600

    register-rbs-form

--- src/components/Register_RBS/Register-RBS.jsx
+++ src/components/Register_RBS/Register-RBS.jsx
@@ -3,12 +3,18 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
 const Register_RBS = () => {
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		const email = event.target.email.value;
+		const password = event.target.password.value;
+		console.log(email, password);
+	};
 	return (
 		<div className='mt-5'>
-			<Form>
+			<Form onSubmit={handleSubmit}>
 				<Form.Group className='mb-3' controlId='formBasicEmail'>
 					<Form.Label>Email address</Form.Label>
-					<Form.Control type='email' placeholder='Enter email' />
+					<Form.Control type='email' name='email' placeholder='Enter email' />
 					<Form.Text className='text-muted'>
 						We'll never share your email with anyone else.
 					</Form.Text>
@@ -16,10 +22,14 @@ const Register_RBS = () => {
 
 				<Form.Group className='mb-3' controlId='formBasicPassword'>
 					<Form.Label>Password</Form.Label>
-					<Form.Control type='password' placeholder='Password' />
+					<Form.Control
+						type='password'
+						name='password'
+						placeholder='Password'
+					/>
 				</Form.Group>
 				<Form.Group className='mb-3' controlId='formBasicCheckbox'>
-					<Form.Check type='checkbox' label='Check me out' />
+					<Form.Check type='checkbox' label='Accept Terms and Conditions' />
 				</Form.Group>
 				<Button variant='primary' type='submit'>
 					Submit
