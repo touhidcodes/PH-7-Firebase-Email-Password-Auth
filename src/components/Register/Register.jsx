edf70a021330703dc21cb98234b63d7131c61425
Touhidur Zaman <touhidcodes@gmail.com>
2023-04-26 19:35:38 +0600

    validate-password

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,41 +1,65 @@
 import React, { useState } from "react";
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import {
+	createUserWithEmailAndPassword,
+	getAuth,
+	signInWithEmailAndPassword,
+} from "firebase/auth";
 import app from "../../Firebase/firebase.config";
 
-const auth = getAuth(app);
-
 const Register = () => {
-	const [email, setEmail] = useState("");
+	const auth = getAuth(app);
+	const [regError, setRegError] = useState("");
+	const [success, setSuccess] = useState("");
+
 	const handleEmailChange = (event) => {
-		console.log(event.target.value);
-		setEmail(event.target.value);
+		// console.log(event.target.value);
 	};
 
 	const handlePasswordChange = (event) => {
-		console.log(event.target.value);
+		// console.log(event.target.value);
 	};
 
 	const handleSubmit = (event) => {
+		// 1. prevent reload
 		event.preventDefault();
+		setSuccess("");
+		setRegError("");
+		// 2. collect form data
 		const email = event.target.email.value;
 		const password = event.target.password.value;
-		console.log(email, password);
-		signInWithEmailAndPassword(auth, email, password)
+		// Validate Password
+		if (!/(?=.*[A-Z].*[A-Z])/.test(password)) {
+			setRegError("Ensure string has two uppercase letters.");
+			return;
+		}
+		if (!/(?=.*[0-9].*[0-9])/.test(password)) {
+			setRegError("Ensure string has two digits.");
+			return;
+		}
+		// 3. create user in firebase
+		createUserWithEmailAndPassword(auth, email, password)
 			.then((result) => {
 				// Signed in
 				const loggedUser = result.user;
 				console.log(loggedUser);
+				setRegError("");
+				event.target.reset();
+				setSuccess("User has been created successfully...");
+				event.target.reset();
 				// ...
 			})
 			.catch((error) => {
 				const errorCode = error.code;
 				const errorMessage = error.message;
+				setRegError(errorMessage);
 				console.error(error);
 			});
 	};
+
 	return (
 		<div className='mt-5'>
 			<h3>Please Register</h3>
+			<p className='text-danger'>{regError}</p>
 			<form action='' onSubmit={handleSubmit}>
 				<input
 					className='mt-3 rounded  p-2'
@@ -44,6 +68,7 @@ const Register = () => {
 					id='email'
 					placeholder='Input your email'
 					onChange={handleEmailChange}
+					required
 				/>
 				<br />
 				<input
@@ -53,6 +78,7 @@ const Register = () => {
 					id='password'
 					placeholder='Input your Password'
 					onBlur={handlePasswordChange}
+					required
 				/>
 				<br />
 				<input
@@ -61,6 +87,7 @@ const Register = () => {
 					value='Register'
 				/>
 			</form>
+			<p className='text-primary mt-3'>{success}</p>
 		</div>
 	);
 };
